b9086b580f59d8b431a7dc5c8117912bf4db987b
itsbillw <itsbillw@penguin>
2020-09-09 23:24:18 +0200

    Flash message for invalid file type

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask, make_response, redirect, render_template, request, url_for
+from flask import Flask, flash, make_response, redirect, render_template, request, url_for
 import pandas as pd
 from sqlalchemy import create_engine
 
 
 app = Flask(__name__)
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
 ALLOWED_EXTENSIONS = {"csv", "xls", "xlsx"}
 engine = create_engine('sqlite:///saved_loader_files.sqlite3', echo=False)
@@ -11,6 +12,10 @@ engine = create_engine('sqlite:///saved_loader_files.sqlite3', echo=False)
 
 @app.route('/')
 def home():
+    # TODO:
+    # 1. Message when no files uploaded
+    # 2. Edit imported file - map columns, replace values
+
     tables = engine.execute("select name from sqlite_master where type = 'table'").fetchall()
     return render_template("index.html", tables=tables)
 
@@ -25,8 +30,9 @@ def view_table(table_name):
 @app.route('/import', methods=['GET', 'POST'])
 def new_import():
     # TODO:
-    # 1. Check file extension - reject anything other than csv, xls, xlsx
+    # 1. Check for valid data
     # 2. Parse file into loader format
+
     if request.method == 'POST':
         import_file = request.files.get('file')
         file_name = request.files['file'].filename.rsplit('.', 1)[0]
@@ -39,8 +45,8 @@ def new_import():
                 df = pd.read_excel(import_file)
             df.to_sql(file_name, con=engine, if_exists='replace')
         else:
-            # TODO - flash message for error
-            print("Unsupported filetype")
+            flash("Unsupported filetype. Please upload csv or excel file")
+            return redirect(url_for('new_import'))
         return redirect(url_for('home'))
     return render_template("import.html")
 
