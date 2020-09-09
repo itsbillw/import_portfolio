b52a13d6582005a98a6acadb0535a70a0ff5753e
itsbillw <itsbillw@penguin>
2020-09-09 15:59:45 +0200

    drop all tables

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, redirect, render_template, request, url_for
 import pandas as pd
 from sqlalchemy import create_engine
 
@@ -18,9 +18,20 @@ def home():
 def new_import():
     if request.method == 'POST':
         df = pd.read_csv(request.files.get('file'))
-        df.to_sql(input_filename, con=engine, if_exists='replace')
-        return render_template("index.html", tables=tables)
+        file_name = request.files['file'].filename.rsplit('.', 1)[0]
+        df.to_sql(file_name, con=engine, if_exists='replace')
+        tables = engine.execute("select name from sqlite_master where type = 'table'").fetchall()
+        return redirect(url_for('home'))
     return render_template("import.html")
+
+@app.route('/delete-all', methods=['GET', 'POST'])
+def delete_tables():
+    if request.method == 'POST':
+        tables = engine.execute("select name from sqlite_master where type = 'table'").fetchall()
+        for table in tables:
+            engine.execute("DROP TABLE {}".format(table[0]))
+        return redirect(url_for('home'))
+    return render_template("delete.html")
 
 
 if __name__ == '__main__':
