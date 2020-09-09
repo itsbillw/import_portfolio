a8d1f6a1ad6b4185d1918eecd28e3bc321e7aca9
itsbillw <itsbillw@penguin>
2020-09-09 20:30:24 +0200

    adding table view

--- app.py
+++ app.py
@@ -8,10 +8,19 @@ app = Flask(__name__)
 
 engine = create_engine('sqlite:///saved_loader_files.sqlite3', echo=False)
 
+
 @app.route('/')
 def home():
     tables = engine.execute("select name from sqlite_master where type = 'table'").fetchall()
     return render_template("index.html", tables=tables)
+
+
+@app.route('/view/<table_name>', methods=("POST", "GET"))
+def view_table(table_name):
+    df = pd.read_sql("select * from {}".format(table_name), con=engine, index_col="index")
+    table_title = table_name.replace("_"," ").title()
+    return render_template('view.html',  tables=[df.to_html(index=False, classes='data')], title=table_title)
+
 
 
 @app.route('/import', methods=['GET', 'POST'])
@@ -24,7 +33,8 @@ def new_import():
         return redirect(url_for('home'))
     return render_template("import.html")
 
-@app.route('/delete-all', methods=['GET', 'POST'])
+
+@app.route('/delete', methods=['GET', 'POST'])
 def delete_tables():
     if request.method == 'POST':
         tables = engine.execute("select name from sqlite_master where type = 'table'").fetchall()
